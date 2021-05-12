f879cce2e26d054e36b81bf600ae382616dbc33a
Kboujraya <1095756998@qq.com>
2021-05-12 18:50:16 +0800

    Update entrypoint.sh

--- Essential-Files/entrypoint.sh
+++ Essential-Files/entrypoint.sh
@@ -2,5 +2,4 @@
 mv /Essential-Files/Aria2Rclone.htpy  /Essential-Files/Aria2Rclone.ipynb
 jupyter nbextension enable --py widgetsnbextension --sys-prefix
 jupyter serverextension enable voila --sys-prefix
-#curl 'https://raw.githubusercontent.com/developeranaz/Aria2-Rclone-Remote-Uploader-HEROKU/main/Essential-Files/d/donate.developeranaz.txt'
 voila --port=$PORT --no-browser  /Essential-Files/Aria2Rclone.ipynb --theme=dark
